50cf2ca12b269322dfa2f2230db96ce939b436e2
sekinat95 <sekinat95@gmail.com>
2020-05-11 22:03:32 +0200

    fix: codacy

--- eyewitness/controllers/eyewitness.controller.js
+++ eyewitness/controllers/eyewitness.controller.js
@@ -8,11 +8,11 @@ exports.insert = (req, res) => {
 };
 
 exports.list = (req, res) => {
-  const limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit) : 10;
+  const limit = req.query.limit && req.query.limit <= 100 ? parseInt(req.query.limit, 10) : 10;
   let page = 0;
   if (req.query) {
     if (req.query.page) {
-      req.query.page = parseInt(req.query.page);
+      req.query.page = parseInt(req.query.page, 10);
       page = Number.isInteger(req.query.page) ? req.query.page : 0;
     }
   }
