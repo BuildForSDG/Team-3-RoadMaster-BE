1af6a2980480de410ba408e939545fcd4febff49
sekinat95 <sekinat95@gmail.com>
2020-05-11 17:22:22 +0200

    fix: codacy issues

--- index.js
+++ index.js
@@ -1,31 +1,29 @@
-const config = require('./common/config/env.config');
-
 const express = require('express');
 
 const app = express();
 
-const EyewitnessRouter = require('./eyewitness/route.config');
-
 const bodyParser = require('body-parser');
 
-app.use(function settings(req, res, next) {
-    res.header('Access-Control-Allow-Origin', '*');
-    res.header('Access-Control-Allow-Credentials', 'true');
-    res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
-    res.header('Access-Control-Expose-Headers', 'Content-Length');
-    res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
-    if (req.method === 'OPTIONS') {
-        return res.send(200);
-    } else {
-        return next();
-    }
+const EyewitnessRouter = require('./eyewitness/route.config');
+
+const config = require('./common/config/env.config');
+
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Credentials', 'true');
+  res.header('Access-Control-Allow-Methods', 'GET,HEAD,PUT,PATCH,POST,DELETE');
+  res.header('Access-Control-Expose-Headers', 'Content-Length');
+  res.header('Access-Control-Allow-Headers', 'Accept, Authorization, Content-Type, X-Requested-With, Range');
+  if (req.method === 'OPTIONS') {
+    return res.send(200);
+  } else {
+    return next();
+  }
 });
 
 app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({extended: true }));
 
 EyewitnessRouter.routesConfig(app);
 
-app.listen(config.port, function () {
-    //console.log('app listening at port %s', config.port);
-});
+app.listen(config.port);
