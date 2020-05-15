ccc59a467ebfe6ab4b367a9ea43f0caf9130d1f8
sekinat95 <sekinat95@gmail.com>
2020-05-16 01:19:47 +0200

    codacy

--- src/routes/UserRoute.js
+++ src/routes/UserRoute.js
@@ -5,7 +5,6 @@ import UsersController from '../controllers/users.controller';
 const router = new Router();
 
 router.post('/user', UsersController.insert);
-
 router.get('/users', UsersController.list);
 
 export default router;
