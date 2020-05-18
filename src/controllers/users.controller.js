9e6f534a0b806bfb8da4ec3338a2850243bb0625
sekinat95 <sekinat95@gmail.com>
2020-05-18 23:36:38 +0200

    codacy

--- src/controllers/users.controller.js
+++ src/controllers/users.controller.js
@@ -7,7 +7,7 @@ const UsersController = {};
 UsersController.insert = (req, res) => {
   const salt = crypto.randomBytes(16).toString('base64');
   const hash = crypto.createHmac('sha512', salt).update(req.body.password).digest('base64');
-  req.body.password = salt + '$' + hash;
+  req.body.password = `${salt}$${hash}`;
   UsersModel.createUser(req.body).then((result) => {
     res.status(201).send({ id: result.id });
   });
