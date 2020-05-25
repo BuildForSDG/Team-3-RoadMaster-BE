22175f931495d6830f343c30e4a155962a592ac7
sekinat95 <sekinat95@gmail.com>
2020-05-25 02:13:58 +0200

    codacy

--- src/controllers/signupController.js
+++ src/controllers/signupController.js
@@ -3,22 +3,30 @@ import encoder from '../utility/passwordEnc';
 // import database method for saving user
 import userModel from '../models/users.model';
 
-const signupController = (req, res, next) => {
- // checks if user already exists
-  userModel.findOne(req.body.email)
-  .then((result) =>{
+const signupController = (req, res) => {
+  userModel.findOne(req.body.email).then((result) => {
     if (result) {
-      res.status(400).send('email already exists')
+      res.status(400).send('user with this email already exists')
     } else {
       const {
-        email, name, password, age, bloodType, genotype, nextofkinNum, emergencyNum,
-        knownIllnesses, state, city, lga, residentialAdd
+        email,
+        name,
+        password,
+        age,
+        bloodType,
+        genotype,
+        nextofkinNum,
+        emergencyNum,
+        knownIllnesses,
+        state,
+        city,
+        lga,
+        residentialAdd
       } = req.body;
-    
       if (!email && !password) {
         res.status(400).json({
-          status: 'error',
-          error: 'Email and password field cannot be empty'
+          status: "error",
+          error: "Email and password field cannot be empty"
         });
         return;
       }
@@ -40,16 +48,11 @@ const signupController = (req, res, next) => {
         residentialAdd,
         creationDate
       };
-    
-      // inside the database operation, store the jwt
-      userModel.createUser(dbData)
-      .then((result1) => {
-        // create a token to send back to the user
+      userModel.createUser(dbData).then((result1) => {
         const token = jwt.sign({
-          sub: 'the user id from the dB'
+          sub: 'the user id from db'
         }, process.env.TOKENKEY, { expiresIn: 1440 });
         const { _id: userId } = result1;
-        // the body to send to front end
         const responseBody = {
           status: 'Success',
           data: {
@@ -62,6 +65,67 @@ const signupController = (req, res, next) => {
       });
     }
   })
-};
+}
+
+// const signupController = (req, res, next) => {
+//  // checks if user already exists
+//   userModel.findOne(req.body.email)
+//   .then((result) =>{
+//     if (result) {
+//       res.status(400).send('email already exists')
+//     } else {
+//       const {
+//         email, name, password, age, bloodType, genotype, nextofkinNum, emergencyNum,
+//         knownIllnesses, state, city, lga, residentialAdd
+//       } = req.body;
+    
+//       if (!email && !password) {
+//         res.status(400).json({
+//           status: 'error',
+//           error: 'Email and password field cannot be empty'
+//         });
+//         return;
+//       }
+//       const hashedPassword = encoder.hash(password, 9);
+//       const creationDate = new Date().toLocaleDateString;
+//       const dbData = {
+//         email,
+//         password: hashedPassword,
+//         name,
+//         age,
+//         bloodType,
+//         genotype,
+//         nextofkinNum,
+//         emergencyNum,
+//         knownIllnesses,
+//         state,
+//         city,
+//         lga,
+//         residentialAdd,
+//         creationDate
+//       };
+    
+//       // inside the database operation, store the jwt
+//       userModel.createUser(dbData)
+//       .then((result1) => {
+//         // create a token to send back to the user
+//         const token = jwt.sign({
+//           sub: 'the user id from the dB'
+//         }, process.env.TOKENKEY, { expiresIn: 1440 });
+//         const { _id: userId } = result1;
+//         // the body to send to front end
+//         const responseBody = {
+//           status: 'Success',
+//           data: {
+//             message: 'Your account has been successfully created',
+//             token,
+//             userId
+//           }
+//         };
+//         res.status(200).json(responseBody);
+//       });
+//     }
+//   })
+// };
 
 export default signupController;
