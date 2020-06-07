c962d5a0b3ddeae7cc8a575c89f8402dc0d6fcf5
tos4christ <gnztrades@gmail.com>
2020-06-07 21:54:45 +0100

    finished sos feature and socket.io connections reaching MVP

--- src/controllers/signupController.js
+++ src/controllers/signupController.js
@@ -16,23 +16,24 @@ const signupController = (req, res) => {
   req.body.password = hashedPassword;
   req.body.creationDate = creationDate;
   // inside the database operation, store the jwt
-  userModel.createUser(req.body).then((result) => {
-    const { _id: userId } = result;
-    // create a token to send back to the user
-    const token = jwt.sign({
-      sub: userId
-    }, process.env.TOKENKEY, { expiresIn: 1440 });
-    // response body to send to frontend
-    const responseBody = {
-      status: 'Success',
-      data: {
-        message: 'Your account has been successfully created',
-        token,
-        userId
-      }
-    };
-    return res.status(201).json(responseBody);
-  })
+  userModel.createUser(req.body)
+    .then((result) => {
+      const { _id: userId } = result;
+      // create a token to send back to the user
+      const token = jwt.sign({
+        sub: userId
+      }, process.env.TOKENKEY, { expiresIn: 1440 });
+      // response body to send to frontend
+      const responseBody = {
+        status: 'Success',
+        data: {
+          message: 'Your account has been successfully created',
+          token,
+          userId
+        }
+      };
+      res.status(201).json(responseBody);
+    })
     .catch((err) => {
       res.status(401).json(err.message);
     });
--- src/server.js
+++ src/server.js
@@ -5,7 +5,7 @@ import userModel from './models/users.model';
 import responderModel from './models/responders.model';
 import findClosestStation from './utility/findClosestStation';
 
-const PORT = process.env.PORT || 3001;
+const PORT = process.env.PORT || 5000;
 const server = http.createServer(app);
 
 // Instantiation of the socket IO on the server
@@ -13,21 +13,15 @@ const io = socketIO(server);
 
 // Function to run when a client connects to the server whereever it is hosted
 io.on('connection', (socket) => {
-  socket.username = '';
-  // console.log(`${socket.username} just connected`);
-
-  // set the socket name to the name of the responder who logged in
+// set the socket name to the name of the responder who logged in
   socket.on('responderSignin', (data) => {
-    responderModel.getRespondersByEmail(data.email).then((responder) => {
-      socket.username = responder.nameOfUnit;
-    });
+    socket.username = data.nameOfUnit;
+    io.sockets.emit('responderNameChange', { name: socket.username });
   });
-
   // set the socket name to the name of the user who logged in
   socket.on('userSignin', (data) => {
     socket.username = data.name;
     io.sockets.emit('usernameChange', { username: socket.username });
-    // userModel.findOne(data.email).then((user) => socket.username = user.name );
   });
 
   // SOS sent from victim
@@ -35,15 +29,19 @@ io.on('connection', (socket) => {
     // data = {userID: 'id', accidentLocation: {lat: '', lon: ''}}
     userModel.findById(data.userID).then((user) => {
       // find the nearest fire station
-      responderModel.getAll((responders) => {
+      responderModel.getAll().then((responders) => {
         // data.accidentLocation should be in format = { lat: 6.3445645, lon: 3.4533255 }
         const closestStation = findClosestStation(data.accidentLocation, responders);
         // send the response team the medical records of the victim
         // the event name should be the name of the station so that socket.IO can target
         // that station's javascript socket connection
-        io.sockets.emit('response', { user, closestStation });
+        io.sockets.emit(`${closestStation}`, { user, accidentLocation: data.accidentLocation });
+        const message = `The closest station to you is ${closestStation}
+                      and it would take 10 minutes to get to your location, Help is on the way`;
+        io.sockets.emit('reply', { message });
       });
-    });
+    })
+      .catch((e) => console.error(e));
   });
 
   // Eyewitness report from observers
@@ -55,9 +53,3 @@ io.on('connection', (socket) => {
 });
 
 server.listen(PORT);
-// Uncomment to log the server processes
-// server.on('listening', () => {
-//   //console.log(`server running on localhost:${PORT}`);
-// });
-
-export default server;
